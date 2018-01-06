09983489377227f7946492dce7af50b620945f26
Garima Jain <garimaj2108@gmail.com>
2018-01-06 11:17:42 -0800

    Created index, js and css files for memory game.

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
+
